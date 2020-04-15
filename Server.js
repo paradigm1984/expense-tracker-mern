bc79feda648771aa835f81742df9190ac2275a1f
Daniel Blanco <dan@danblanco.com>
2020-04-15 19:39:59 -0400

    connecting mlab DB

--- Server.js
+++ Server.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db')
+
+const transactions = require('./routes/transaction_routes');
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+connectDB();
 
+// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/api/transactions', transactions);
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
 app.listen(port, () => {
- console.log(`server is running on port: ${port}`)
+ console.log(`server is running on port: ${port}`.blue.bold)
 });
