62a4e06455fb6b5785fb93af8697cdec5c731fd0
Daniel Blanco <dan@danblanco.com>
2020-04-15 13:06:56 -0400

    tweaking package.json file to run both react and node. works on local.

--- Server.js
+++ Server.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.get('/*', (req, res) => {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
