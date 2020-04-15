e1bbab770a44b9e53cfec652e0bae6b92c206606
Daniel Blanco <dan@danblanco.com>
2020-04-15 13:04:07 -0400

    tweaking package.json file to run both react and node. works on local.

---
+++ client/build/precache-manifest.e98c25c67f5108d08ac539701abbf40f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285c2b1d7a12138760635cd93e6ad75b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00da970352d2b6af09d2",
+    "url": "/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "82a616e69628dafcceb9",
+    "url": "/static/js/2.9124e96c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9124e96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00da970352d2b6af09d2",
+    "url": "/static/js/main.04395a7d.chunk.js"
+  },
+  {
+    "revision": "3e5e20c52f8e9bacf785",
+    "url": "/static/js/runtime-main.b6a55b0e.js"
+  }
+]);
