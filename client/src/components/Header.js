7e24a3b469bca442d9797ccf6c44492d1f785013
Daniel Blanco <dan@danblanco.com>
2020-06-24 17:28:23 -0400

    added some additional styling and functionality to the navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,17 +7,19 @@ import '../styles/Header.scss';
 
 export const Header = () => {
  return (
-  <div className="container">
-   <Navbar expand="sm">
-    <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-   <Nav className="ml-auto">
-      <Nav.Link href="ttps://github.com/paradigm1984/expense-tracker-mern">Github Repo</Nav.Link>
-      <Nav.Link href="https://danblanco.com">Portfolio</Nav.Link>
-   </Nav>
-  </Navbar.Collapse>
-  </Navbar >
+  <div className="nav-wrapper">
+   <div className="container">
+    <Navbar id="navigation" expand="sm">
+     <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
+   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+   <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto">
+       <Nav.Link href="ttps://github.com/paradigm1984/expense-tracker-mern">Github Repo</Nav.Link>
+       <Nav.Link href="https://danblanco.com">Portfolio</Nav.Link>
+    </Nav>
+   </Navbar.Collapse>
+   </Navbar >
+    </div>
   </div>
  )
 }
