5b5f0fdb1ea37136870c563158e6bbd3fe6790ab
Daniel Blanco <dan@danblanco.com>
2020-07-02 09:22:46 -0400

    added footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+// Footer.js
+
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer = () => {
+
+ return (
+  <footer className="page-footer font-small">
+   <div className="footer-copyright text-center py-3">© 2020 Copyright:
+    <a className="copyright-link" href="https://danblanco.com/"> danblanco.com</a>
+   </div>
+  </footer>
+
+ );
+}
+
+export default Footer;
